ba6fc262132655291aa68b4bdd094bea882dac93
Paul Mc Gonigle <S00210326@mail.itsligo.ie>
2022-05-17 09:39:22 +0100

    Created Class

---
+++ MayJuneFinalExam/RentalProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayJuneFinalExam
+{
+    public enum RentalType//enum
+    {
+        House,
+        Flat,
+        Share
+    }
+    public class RentalProperty//class for property
+    {
+        //properties
+        public int ID { get; set; }
+
+        public RentalType RentalType { get; set; }
+
+        public string Location { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        //constructors
+
+        //methods
+
+    }//end of class
+}
